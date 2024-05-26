23c3092c86d7ad18ac3716d20d9f489291a7d168
Lucreciaheffel <heffellucrecia@gmail.com>
2024-05-25 21:38:17 -0300

    modificaciones en todas las carpetas, html, css y js

---
+++ script.js
@@ -0,0 +1,22 @@
+/*let color = prompt("Ingresa tu color favorito en minusculas");
+
+if (color.toLowerCase() === "rosa") {
+  alert("Wow! Vas a disfrutar mucho de esta pagina! Bienvenida/o");
+} 
+else{
+    alert (" CUIDADO! Esta pagina te puede resultar empalagosa");
+}
+
+let nom = prompt("Ingresa tu nombre");
+function saludo() {
+    if (nom) {
+        alert("bienvenida/o " + nom);
+    }
+    else {
+        alert("tenes que ingresar un nombre");
+    }
+}
+saludo();
+
+alert("SE FELIZ EN UN MUNDO ROSA!");
+*/
