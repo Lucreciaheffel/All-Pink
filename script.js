714de51e48c93e498f111073a04850df4c9ec65b
Lucreciaheffel <heffellucrecia@gmail.com>
2024-05-29 14:41:09 -0300

    se agregaron comentarios en los codigos

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+/* Al ingresar al sitio pide que se ingrese el color favorito da acceso si el color favorito es rosa y si es otro alerta que tenga cuidado*/
 let color = prompt("Ingresa tu color favorito");
-
 if (color.toLowerCase() === "rosa") {
   alert("Wow! Vas a disfrutar mucho de esta pagina! Bienvenida/o");
 } 
 else{
     alert (" CUIDADO! Esta pagina te puede resultar empalagosa");
 }
-
+/*funcion saludo*/
 let nom = prompt("Ingresa tu nombre");
 function saludo() {
     if (nom) {
@@ -17,6 +17,6 @@ function saludo() {
     }
 }
 saludo();
-
+/*ventana de alerta*/
 alert("SE FELIZ EN UN MUNDO ROSA!");
 
